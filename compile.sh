3b5f2ecc084b45fd53e1d87614b6f87e4c4a41a1
Tyler Hayes <tgh@pdx.edu>
2011-03-17 16:28:30 -0700

    Modify compile.sh to not compile TestDriver.java

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-javac -sourcepath ./src/ -d ./build/classes/ ./src/tylerhayes/tools/*.java
+src_path=~/Projects/JavaTools/src/tylerhayes/tools
+
+javac -sourcepath ./src/ -d ./build/classes/ $src_path/Log.java $src_path/GracefulShutdown.java $src_path/DatabaseConnector.java $src_path/PostgreSQLConsole.java $src_path/TerminationInterceptor.java
