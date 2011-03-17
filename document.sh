8150e1096f0ea4cf30916a54a78881faa83acc5a
Tyler Hayes <tgh@pdx.edu>
2011-03-17 16:26:15 -0700

    Modify document.sh to not include the TestDriver

--- document.sh
+++ document.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-javadoc -d ./doc/ ./src/tylerhayes/tools/*.java
+src_path=~/Projects/JavaTools/src/tylerhayes/tools
+
+javadoc -d ./doc/ $src_path/Log.java $src_path/GracefulShutdown.java $src_path/DatabaseConnector.java $src_path/PostgreSQLConsole.java $src_path/TerminationInterceptor.java
