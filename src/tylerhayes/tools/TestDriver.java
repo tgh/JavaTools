16a372f52a5cf4c853722827d63910ee7c207a8c
Tyler Hayes <tgh@pdx.edu>
2011-03-17 16:07:38 -0700

    Add TestDriver.java

---
+++ src/tylerhayes/tools/TestDriver.java
@@ -0,0 +1,23 @@
+package tylerhayes.tools;
+
+public class TestDriver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Log log = new Log("/home/tgh/workspace/JavaTools/test.log");
+		log.logHeader("START");
+		log.logGeneralMessage("Nothing to report.", 0, false);
+		log.enableStdoutForAll();
+		log.logData("speed = FULL", 0, false);
+		log.logError("Oops.", 0, false);
+		log.disableStdoutForAll();
+		log.enableStdoutForErrorsOnly();
+		log.logWarning("Warning. Iceberg ahead.", 0, false);
+		log.logFatalError("We're dead.", 0, false);
+		log.logFooter("END");
+		log.close();
+	}
+
+}
